a7fac20593af6e40865f18d78c24ed9acd9f469b
Spencer Baynton <spencertbaynton@gmail.com>
2013-03-02 03:47:27 -0500

    Apply a default data-stellar-background-ratio via JavaScript

--- js/script.js
+++ js/script.js
@@ -2,6 +2,20 @@ jQuery(document).ready(function ($) {
 
     $('body').scrollspy({target: '[role="banner"]'});
 
+    // parallax these element's `background-image`
+    parallax = $('.slide');
+
+    parallax.each(function() {
+        // if the element has a `background-image`
+        if ( $(this).css('background-image') != 'none' ) {
+            // and a `data-stellar-background-ratio` has not been defined
+            if ( !( $(this).data('stellar-background-ratio') ) ) {
+                // apply a default `data-stellar-background-ratio`
+                $(this).attr('data-stellar-background-ratio', '0.5');
+            }
+        }
+    });
+
     $.stellar();
 
     anchors = $('a[href^=#]');
